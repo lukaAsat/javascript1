efda7d09a4ce8e5d01b8f2214ae00fd4a8a78ed1
lukaAsat <131876969+lukaAsat@users.noreply.github.com>
2023-06-16 23:25:24 +0400

    Add files via upload
    javascript1

---
+++ script.js
@@ -0,0 +1,43 @@
+let x=100
+
+if (x <50 )  {
+
+    console.log(x < 50);
+
+} else if ( x > 20) {
+
+    console.log(x>20);
+}  
+ 
+else {
+    console.log("error");
+}
+
+
+
+// იწყება დავალება 2
+
+let y = 'mariam'
+
+if (y=='mariam') {
+
+console.log('true');
+
+} else {
+   console.log( 'false');
+}
+
+
+
+// იწყება დავალება 3 
+
+const z= 'mariami' ;
+switch (z) {
+      case 'mariami':
+      console.log( 'true' );
+      break;
+      default :
+      console.log('false');
+}
+
+
